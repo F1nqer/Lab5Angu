1282b00d18716bfe78874a65c6231a1d617d96ba
F1nqer <70800656+F1nqer@users.noreply.github.com>
2020-10-17 03:38:03 +0600

    lab5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-color',
+  template: `<div [style.backgroundColor]="colors[0].hex">
+              <input type="text" [(ngModel)]="colors[0].hex" />
+              <input type="text" [(ngModel)]="colors[0].name" />
+            </div>
+            <div [style.background-color]="colors[1].hex">
+              <input type="text" [(ngModel)]="colors[1].hex" />
+              <input type="text" [(ngModel)]="colors[1].name" />
+            </div>
+            <div [style.background-color]="colors[2].hex">
+              <input type="text" [(ngModel)]="colors[2].hex" />
+              <input type="text" [(ngModel)]="colors[2].name" />
+            </div>
+            <div [style.background-color]="colors[3].hex">
+              <input type="text" [(ngModel)]="colors[3].hex" />
+              <input type="text" [(ngModel)]="colors[3].name" />
+            </div>
+            <div [style.background-color]="colors[4].hex">
+              <input type="text" [(ngModel)]="colors[4].hex" />
+              <input type="text" [(ngModel)]="colors[4].name" />
+            </div>
+            `,
+  styles: [`
+    div {
+      width: 10000px;
+      height: 150px;
+      border: 4px solid #000000
+    }
+  `]
 })
 export class AppComponent {
-  title = 'lab4';
+  colors: Color[] = [new Color('#bd9bff', 'purple'),
+    new Color('#f95734', 'red-orange'),
+    new Color('#7a9dea', 'sky blue'),
+    new Color('#e3e829', 'yellow-green'),
+    new Color('#950a24', 'very red')];
 }
+class Color{
+  hex: string;
+  name: string;
+  constructor(hex: string, name: string) {
+    this.hex = hex;
+    this.name = name;
+  }
+}
+
+const first: Color = new Color('fsdfds', 'fsdfds');
